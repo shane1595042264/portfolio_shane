64c0e23ddffc766bef3ceef6b05ec76b7af2d0ea
Shane Li <61711097+shane1595042264@users.noreply.github.com>
2022-04-05 09:38:10 -0400

    style

---
+++ frontend_react/src/container/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import "./Header.scss"
+import {motion} from 'framer-motion'
+import {images} from '../../constants'
+const Header = () => {
+  return (
+    <div className='app__header app_flex'>
+    <motion.div>
+      whileInView
+    </motion.div>
+    </div>
+  )
+}
+
+export default Header
